84b160487986b22cd8b5dfde45ad2c27f73e3913
Bankole Samuel <bankolesamuel175@gmail.com>
2020-01-29 10:38:57 +0100

    learn how to use slot

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,29 @@
 <template>
   <div>
-    <app-header :title="title" @changeTitle="updateTitle($event)" />
-
-    <app-ninjas :ninjas="ninjas" />
-    <hr />
-    <app-ninjas :ninjas="ninjas" />
-
-    <app-footer :title="title" />
+    <form-helper>
+      <div slot="form-header">
+        <h3>Form Title</h3>
+        <p>Information Field</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button @click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Ninjas from "./components/Ninjas";
+import FormHelper from "./components/FormHelper";
 export default {
   components: {
-    "app-header": Header,
-    "app-footer": Footer,
-    "app-ninjas": Ninjas
+    FormHelper
   },
   data() {
-    return {
-      ninjas: [
-        { id: 1, name: "Ryan", specialty: "Vue Component", show: false },
-        { id: 2, name: "Cras", specialty: "HTML Wizard", show: false },
-        { id: 3, name: "Ryan", specialty: "Click Event", show: false },
-        { id: 4, name: "Ryan", specialty: "Conditiional", show: false },
-        { id: 5, name: "Ryan", specialty: "Webpack Component", show: false },
-        { id: 6, name: "Ryan", specialty: "Data Digging", show: false }
-      ],
-
-      title: "Vue Ninja Banky"
-    };
-  },
-
-  methods: {
-    updateTitle(updatedTitle) {
-      this.title = updatedTitle;
-    }
+    return {};
   }
 };
 </script>
